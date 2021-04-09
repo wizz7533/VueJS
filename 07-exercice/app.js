4b7de0b1e16aed848700152e56533ac0db13b05f
Denis Fortict <d.fortict@vivit.fr>
2021-04-09 17:12:12 +0200

    exercice boucle et rendu

---
+++ 07-exercice/app.js
@@ -0,0 +1,9 @@
+import { Employe } from './classes/employe.js'
+let app = Vue.createApp({
+    data() {
+
+        return {
+            utilisateurs: Employe.makeTenEmploye()
+        }
+    }
+}).mount('#app');
