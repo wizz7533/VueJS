61a74f631e94b9e03391aef792c57e7a20915ecf
Denis Fortict <d.fortict@vivit.fr>
2021-04-12 14:36:16 +0200

    ajout de l'attribut alt

--- 11-Exercices/11-01-Gestion Evenements/app.js
+++ 11-Exercices/11-01-Gestion Evenements/app.js
@@ -1,17 +1,21 @@
 let app = Vue.createApp({
     data() {
         return {
-            imgUrl: ''
+            imgUrl: '',
+            imgAlt: ''
         }
     },
     methods: {
         changeImage(nombre) {
             if (nombre == 1) {
                 this.imgUrl = "./images/dog.jpg"
+                this.imgAlt = "A dog"
             } else if (nombre == 2) {
                 this.imgUrl = "./images/cat.jpg"
+                this.imgAlt = "A cat"
             } else if (nombre == 3) {
                 this.imgUrl = "./images/rabbit.jpg"
+                this.imgAlt = "A rabbit"
             } else {
                 imgUrl = " "
             };
