eebc943aa6c5a9dc98d1e64b02d7d5183e2449f3
Denis Fortict <d.fortict@vivit.fr>
2021-04-12 12:53:38 +0200

    fin d'ex

--- 11-Exercices/11-01-Gestion Evenements/app.js
+++ 11-Exercices/11-01-Gestion Evenements/app.js
@@ -1,10 +1,21 @@
 let app = Vue.createApp({
     data() {
         return {
-            imgUrl: "",
+            imgUrl: " ",
         }
     },
     methods: {
+        changeImage(nombre) {
+            if (nombre == 1) {
+                this.imgUrl = "./images/dog.jpg"
+            } else if (nombre == 2) {
+                this.imgUrl = "./images/cat.jpg"
+            } else if (nombre == 3) {
+                this.imgUrl = "./images/rabbit.jpg"
+            } else {
+                imgUrl = " "
+            };
+        }
 
     }
 });
