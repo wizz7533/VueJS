c8544091b6d9765ad9685ab3977a36a67158bbc5
Denis Fortict <d.fortict@vivit.fr>
2021-04-12 17:14:26 +0200

    correction

--- 11-Exercices/11-02-01-TodoList-Correstion/app.js
+++ 11-Exercices/11-02-01-TodoList-Correstion/app.js
@@ -1,17 +1,35 @@
 let app = Vue.createApp({
     data() {
         return {
-            todos: []
+            todos: [],
+            options: [
+                { label: 'Urgent', type: 'alert alert-danger' },
+                { label: 'Important', type: 'alert alert-warning' },
+                { label: 'Pas important', type: 'alert alert-success' }
+            ],
         }
     },
     methods: {
-        adTask(event) {
-            let text = event.target.task.value;
-            this.todos.push(texte);
-            event.preventDefault();
+        addTask(event) {
+            let texte = event.target.task.value;
+            let taille = event.target.size.value;
 
+            // console.log("Texte:" + texte);
+            // console.log("Taille: " + taille);
+            // console.log("Couleur:" + event.target.level.value);
+
+            let task = {
+                texte: texte,
+                style: {
+                    fontSize: `${taille}px`,
+                    listStyle: 'none'
+                },
+                couleur: event.target.level.value
+            }
+
+            this.todos.push(task);
         }
-
     }
 });
-app.mount('#app');
+
+app.mount("#app");
