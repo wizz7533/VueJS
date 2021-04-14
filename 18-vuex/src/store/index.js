2dc625e4235920d4b7ba1e1453ccb7d846081bab
Denis Fortict <d.fortict@vivit.fr>
2021-04-14 17:09:25 +0200

    explication vuex & ajout getter dans store/index.js

---
+++ 18-vuex/src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+    // Equivalent à Data
+    state: {},
+    // Responsable du changement d'Etat de l'application
+    mutations: {},
+    // Equivalent aux methods ( traitement complexe ou asyncrone, puis appelle la mutation)
+    actions: {},
+    // Equivalent des propriétés calculées
+    getter: {},
+    // Equivalent à components: {}
+    modules: {}
+})
